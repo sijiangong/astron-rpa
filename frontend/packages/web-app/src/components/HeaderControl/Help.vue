7ffbe6db65190a686333dd2dc1c95ef6a7b39943
jack <jack@nodomain.com>
2026-09-10 10:58:49 +0800

    chore(header): 隐藏帮助菜单中的「投诉举报」入口
    仅移除该入口（帮助菜单保留「帮助中心」）；ComplaintModal 组件、aiFeedback 接口与 locale 文案保留，便于日后恢复。

--- frontend/packages/web-app/src/components/HeaderControl/Help.vue
+++ frontend/packages/web-app/src/components/HeaderControl/Help.vue
@@ -1,7 +1,4 @@
 <script setup lang="ts">
-import { NiceModal } from '@rpa/components'
-
-import { ComplaintModal } from '@/components/ComplaintModal'
 import { VUE_APP_HELP } from '@/constants'
 import { utilsManager } from '@/platform'
 
@@ -9,10 +6,6 @@ import ControlButton from './ControlButton.vue'
 
 function handleHelpInfo() {
   utilsManager.openInBrowser(VUE_APP_HELP)
-}
-
-function handleComplaint() {
-  NiceModal.show(ComplaintModal)
 }
 </script>
 
@@ -26,9 +19,6 @@ function handleComplaint() {
       <a-menu>
         <a-menu-item @click="handleHelpInfo">
           {{ $t('helperCenter') }}
-        </a-menu-item>
-        <a-menu-item @click="handleComplaint">
-          {{ $t('complaint') }}
         </a-menu-item>
       </a-menu>
     </template>
